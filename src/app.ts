ee838fe7bc7f07faf1eb53005f257ffde8a3a8cc
wuzisen <jonsen.wu@coolkit.cn>
2019-10-15 18:25:18 +0800

    迁移提交

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
+export default app;
